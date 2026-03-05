1ed81f2c6896fb9abce2559ffecb0fa33c182d85
ano1her-project <ano1her.project@gmail.com>
2026-03-05 22:33:00 +0100

    bitboard ShiftLeft, ShiftRight
    MK.4 IS COMING

--- Osmium.Core/Core.cs
+++ Osmium.Core/Core.cs
@@ -2,17 +2,22 @@
 
 public class BitboardOperations
 {
+    static readonly ulong aFile = 1ul | (1ul << 8) | (1ul << 16) | (1ul << 24) | (1ul << 32) | (1ul << 40) | (1ul << 48) | (1ul << 56);
+    static readonly ulong notAFile = ~aFile;
+    static readonly ulong hFile = (1ul << 7) | (1ul << 15) | (1ul << 23) | (1ul << 31) | (1ul << 39) | (1ul << 47) | (1ul << 55) | (1ul << 63);
+    static readonly ulong notHFile = ~hFile;
+
     public static ulong ShiftUp(ulong bitboard)
         => bitboard << 8;
-
-    public static ulong ShiftUp(ulong bitboard, int shiftAmount)
-        => bitboard << (8 * shiftAmount);
 
     public static ulong ShiftDown(ulong bitboard)
         => bitboard >> 8;
 
-    public static ulong ShiftDown(ulong bitboard, int shiftAmount)
-        => bitboard >> (8 * shiftAmount);
+    public static ulong ShiftRight(ulong bitboard) // prevents pieces at the edges from wrapping around
+        => (bitboard & notHFile) >> 1;
+
+    public static ulong ShiftLeft(ulong bitboard) // prevents pieces at the edges from wrapping around
+        => (bitboard & notAFile) << 1;
 }
 
 public enum PieceType
