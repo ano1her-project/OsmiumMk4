148c3c6d87ad8b1e3e0ef11c31c900d9ee902b16
ano1her-project <ano1her.project@gmail.com>
2026-03-06 18:35:56 +0100

    created move struct

---
+++ Osmium.Core/Move.cs
@@ -0,0 +1,12 @@
+﻿namespace Osmium.Core;
+
+readonly struct Move
+{
+    public readonly int from, to;
+
+    public Move(int p_from, int p_to)
+    {
+        from = p_from;
+        to = p_to;
+    }
+}
