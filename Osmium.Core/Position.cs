24d81f4b70851bc9d40a7607b75e59e555db5ba1
ano1her-project <ano1her.project@gmail.com>
2026-03-09 16:05:51 +0100

    one generalized method for bishops, rooks, queens
    GetSliderMoves()

--- Osmium.Core/Position.cs
+++ Osmium.Core/Position.cs
@@ -86,48 +86,6 @@ public class Position
         return result;
     }
 
-    public List<Move> GetBishopMoves(PieceColor bishopColor)
-    {
-        var bishops = GetPieceOfColorBitboard(PieceType.Bishop, bishopColor);
-        List<Move> result = [];
-        while (bishops != 0)
-        {
-            bishops = Bitboards.PopLeastSignificantOne(bishops, out int from);
-            for (Direction direction = Direction.Northeast; (int)direction < 8; direction += 2)
-            {
-                // Let's assume the bishop is at b1 and a blocking piece is at f5.
-                // The ray bitboard will contain all squares in the line from c2 to h7.
-                // The blocker will equal 37 (f5).
-                // blockerBitboard - 1 will contain all squares with an index less than 37.
-                // Thus, the targets bitboard will contain all squares in the line from c2 to e4 including e4.
-                var ray = Bitboards.GetRayMask(direction, from);
-                var piecesOnRay = ray & ~emptySquareSet;
-                ulong targets;
-                if (piecesOnRay != 0)
-                {
-                    bool directionIsPositive = Bitboards.IsPositive(direction);
-                    int blocker = directionIsPositive ?               // from Bitboards.cs:
-                        Bitboards.LeastSignificantOne(piecesOnRay) : // "positive" directions correspond to left shifts and the first hit is the ls1b
-                        Bitboards.MostSignificantOne(piecesOnRay);  // "negative" directions correspond to right shifts and the first hit is the ms1b
-                    var blockerMask = Bitboards.squareToMask[blocker];
-                    targets = (directionIsPositive ? (blockerMask - 1) : ~((blockerMask << 1) - 1)) & ray;
-                    // "bonus" check on top - if the blocker is an enemy, add a capture move
-                    var enemyColor = PieceColors.Opposite(bishopColor);
-                    if ((blockerMask & GetColorBitboard(enemyColor)) != 0) // the blocker bitboard "survives" the enemy color mask = the blocker is an enemy
-                        result.Add(new(from, blocker, PieceType.Bishop, true));
-                }
-                else // if there is no blocker
-                    targets = ray; 
-                while (targets != 0)
-                {
-                    targets = Bitboards.PopLeastSignificantOne(targets, out int target);
-                    result.Add(new(from, target, PieceType.Bishop, false));
-                }
-            }
-        }        
-        return result;
-    }
-
     public List<Move> GetKnightMoves(PieceColor knightColor)
     {
         var knights = GetPieceOfColorBitboard(PieceType.Knight, knightColor);
@@ -152,21 +110,25 @@ public class Position
         return result;
     }
 
-    public List<Move> GetRookMoves(PieceColor rookColor)
+    List<Move> GetSliderMoves(PieceColor sliderColor, PieceType pieceType, Direction startAtDirection, int directionIncrement)
     {
-        var bishops = GetPieceOfColorBitboard(PieceType.Rook, rookColor);
+        var sliders = GetPieceOfColorBitboard(pieceType, sliderColor);
         List<Move> result = [];
-        while (bishops != 0)
+        while (sliders != 0)
         {
-            bishops = Bitboards.PopLeastSignificantOne(bishops, out int from);
-            for (Direction direction = Direction.North; (int)direction < 8; direction += 2)
+            sliders = Bitboards.PopLeastSignificantOne(sliders, out int from);
+            for (Direction direction = startAtDirection; (int)direction < 8; direction += directionIncrement)
             {
                 var ray = Bitboards.GetRayMask(direction, from);
                 var piecesOnRay = ray & ~emptySquareSet;
                 ulong targets;
                 if (piecesOnRay != 0)
                 {
-                    // see GetBishopMoves() comments
+                    // Say the slider is a bishop at b1 and a blocking piece is at f5.
+                    // The ray bitboard will contain all squares in the line from c2 to h7.
+                    // The blocker will equal 37 (f5).
+                    // blockerBitboard - 1 will contain all squares with an index less than 37.
+                    // Thus, the targets bitboard will contain all squares in the line segment from c2 to e4 including e4.
                     bool directionIsPositive = Bitboards.IsPositive(direction);
                     int blocker = directionIsPositive ?               // from Bitboards.cs:
                         Bitboards.LeastSignificantOne(piecesOnRay) : // "positive" directions correspond to left shifts and the first hit is the ls1b
@@ -174,64 +136,35 @@ public class Position
                     var blockerMask = Bitboards.squareToMask[blocker];
                     targets = (directionIsPositive ? (blockerMask - 1) : ~((blockerMask << 1) - 1)) & ray;
                     // "bonus" check on top - if the blocker is an enemy, add a capture move
-                    var enemyColor = PieceColors.Opposite(rookColor);
+                    var enemyColor = PieceColors.Opposite(sliderColor);
                     if ((blockerMask & GetColorBitboard(enemyColor)) != 0) // the blocker bitboard "survives" the enemy color mask = the blocker is an enemy
-                        result.Add(new(from, blocker, PieceType.Rook, true));
+                        result.Add(new(from, blocker, pieceType, true));
                 }
                 else // if there is no blocker
                     targets = ray;
                 while (targets != 0)
                 {
                     targets = Bitboards.PopLeastSignificantOne(targets, out int target);
-                    result.Add(new(from, target, PieceType.Rook, false));
+                    result.Add(new(from, target, pieceType, false));
                 }
             }
         }
         return result;
     }
 
+    public List<Move> GetBishopMoves(PieceColor bishopColor)
+        => GetSliderMoves(bishopColor, PieceType.Bishop, Direction.Northeast, 2); // only odd directions (diagonals)
+
+    public List<Move> GetRookMoves(PieceColor rookColor)
+        => GetSliderMoves(rookColor, PieceType.Rook, Direction.North, 2); // only even directions (cardinals)
+
     public List<Move> GetQueenMoves(PieceColor queenColor)
-    {
-        var bishops = GetPieceOfColorBitboard(PieceType.Queen, queenColor);
-        List<Move> result = [];
-        while (bishops != 0)
-        {
-            bishops = Bitboards.PopLeastSignificantOne(bishops, out int from);
-            for (Direction direction = Direction.North; (int)direction < 8; direction++)
-            {
-                var ray = Bitboards.GetRayMask(direction, from);
-                var piecesOnRay = ray & ~emptySquareSet;
-                ulong targets;
-                if (piecesOnRay != 0)
-                {
-                    // see GetBishopMoves() comments
-                    bool directionIsPositive = Bitboards.IsPositive(direction);
-                    int blocker = directionIsPositive ?               // from Bitboards.cs:
-                        Bitboards.LeastSignificantOne(piecesOnRay) : // "positive" directions correspond to left shifts and the first hit is the ls1b
-                        Bitboards.MostSignificantOne(piecesOnRay);  // "negative" directions correspond to right shifts and the first hit is the ms1b
-                    var blockerMask = Bitboards.squareToMask[blocker];
-                    targets = (directionIsPositive ? (blockerMask - 1) : ~((blockerMask << 1) - 1)) & ray;
-                    // "bonus" check on top - if the blocker is an enemy, add a capture move
-                    var enemyColor = PieceColors.Opposite(queenColor);
-                    if ((blockerMask & GetColorBitboard(enemyColor)) != 0) // the blocker bitboard "survives" the enemy color mask = the blocker is an enemy
-                        result.Add(new(from, blocker, PieceType.Queen, true));
-                }
-                else // if there is no blocker
-                    targets = ray;
-                while (targets != 0)
-                {
-                    targets = Bitboards.PopLeastSignificantOne(targets, out int target);
-                    result.Add(new(from, target, PieceType.Queen, false));
-                }
-            }
-        }
-        return result;
-    }
+        => GetSliderMoves(queenColor, PieceType.Queen, Direction.North, 1); // all directions
 
     public List<Move> GetKingMoves(PieceColor kingColor)
     {
-        var kings = GetPieceOfColorBitboard(PieceType.King, kingColor);
-        int king = Bitboards.LeastSignificantOne(kings);        
+        var kingMask = GetPieceOfColorBitboard(PieceType.King, kingColor);
+        int king = Bitboards.LeastSignificantOne(kingMask);        
         var targets = Bitboards.kingMoves[king] & emptySquareSet;
         List<Move> result = [];
         while (targets != 0)
@@ -264,7 +197,7 @@ public class Position
         ];
     }
 
-    //
+    // making and unmaking moves:
 
     public void MakeMove(Move move, out UndoInfo undoInfo)
     {
