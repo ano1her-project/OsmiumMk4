c6d781287ffc1a728da07a027ff3275626ae6671
ano1her-project <ano1her.project@gmail.com>
2026-03-18 21:49:22 +0100

    split off file test files

---
+++ Osmium.Tests/PerftTests.cs
@@ -0,0 +1,17 @@
+﻿using Osmium.Core;
+using Osmium.Minimax;
+
+namespace Osmium.Tests;
+
+public class PerftTests
+{
+    [Theory]
+    [InlineData(1, 20u)]
+    [InlineData(2, 400u)]
+    [InlineData(3, 8_902u)]
+    [InlineData(4, 197_281u)]
+    [InlineData(5, 4_865_609u)]
+    [InlineData(6, 119_060_324u)]
+    public void StartingPositionPerft(int depth, uint expected)
+        => Assert.Equal(expected, Perft.CountLeafNodesAtDepth(Position.StartingPosition(), depth));
+}
